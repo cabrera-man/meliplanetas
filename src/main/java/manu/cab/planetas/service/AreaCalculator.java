ef483b492b6511810808d99d86fa91c46a9be215
Manuel Cabrera <casiodc@gmail.com>
2019-05-19 15:29:42 -0300

    Added pointInTriangle

--- src/main/java/manu/cab/planetas/service/AreaCalculator.java
+++ src/main/java/manu/cab/planetas/service/AreaCalculator.java
@@ -1,5 +1,7 @@
 package manu.cab.planetas.service;
 
+import java.awt.geom.Point2D;
+
 import org.springframework.stereotype.Service;
 
 import manu.cab.planetas.domain.Planeta;
@@ -17,13 +19,34 @@ public class AreaCalculator {
 		return result;
 	}
 	
-	public double perimetro(int dia, Planeta planetaA, Planeta planetaB, Planeta planetaC) {
+	public double perimeter(int dia, Planeta planetaA, Planeta planetaB, Planeta planetaC) {
 		
 		double distPAPB = Math.sqrt(Math.pow(planetaB.getPoint2D(dia).getX() - planetaA.getPoint2D(dia).getX(), 2d) + Math.pow(planetaB.getPoint2D(dia).getY() - planetaA.getPoint2D(dia).getY(), 2d));
 		double distPBPC = Math.sqrt(Math.pow(planetaC.getPoint2D(dia).getX() - planetaB.getPoint2D(dia).getX(), 2d) + Math.pow(planetaC.getPoint2D(dia).getY() - planetaB.getPoint2D(dia).getY(), 2d));
 		double distPCPA = Math.sqrt(Math.pow(planetaA.getPoint2D(dia).getX() - planetaC.getPoint2D(dia).getX(), 2d) + Math.pow(planetaA.getPoint2D(dia).getY() - planetaC.getPoint2D(dia).getY(), 2d));
 		
 		return distPAPB + distPBPC + distPCPA;
+	}
+	
+	public boolean pointInTriangle(int dia, Planeta planetaA, Planeta planetaB, Planeta planetaC, Planeta sol)
+	{
+		//Honestidad 100%
+		//https://www.youtube.com/watch?v=HYAgJN3x4GA
+		//https://github.com/SebLague/Gamedev-Maths/blob/master/PointInTriangle.cs
+		Point2D posPA = planetaA.getPoint2D(dia);
+		Point2D posPB = planetaB.getPoint2D(dia);
+		Point2D posPC = planetaC.getPoint2D(dia);
+		Point2D posSun = sol.getPoint2D(dia);
+		
+		double s1 = posPC.getY() - posPA.getY();
+		double s2 = posPC.getX() - posPA.getX();
+		double s3 = posPB.getY() - posPA.getY();
+		double s4 = posSun.getY() - posPA.getY();
+		
+		double w1 = (posPA.getX() * s1 + s4 * s2 - posSun.getX() * s1) / (s3 * s2 - (posPB.getX()-posPA.getX()) * s1);
+		
+		double w2 = (s4- w1 * s3) / s1;
+		return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
 	}
 
 }
--- src/test/java/manu/cab/planetas/service/AreaCalculatorTest.java
+++ src/test/java/manu/cab/planetas/service/AreaCalculatorTest.java
@@ -32,27 +32,31 @@ public class AreaCalculatorTest {
 	public void test2() {
 		int countCollinear = 0;
 		int countSequia = 0;
+		int countDiasLluvia = 0;
+		
 		double signumPrevPlanet = -1f;
 		double signumPrevSun = -1f;
 		
 		Double maxPerimetro = new Double(0d);
 		
 		for(int n = 1; n < 365*10; n++) {
-			double perimetroPlanet = ac.perimetro(n, pA, pB, pC);
-			
 			double areaPlanet = ac.area(n, pA, pB, pC);
 			double areaABS = Math.abs(areaPlanet);
 			
 			double areaSun = ac.area(n, pA, pB, sol);
 			
+			boolean isSunInsideTriangle = ac.pointInTriangle(n, pA, pB, pC, sol);
+			if(isSunInsideTriangle) {
+				countDiasLluvia++;
+				double perimetroPlanet = ac.perimeter(n, pA, pB, pC);
+				//Max perimetro del triangulo
+				if(maxPerimetro.compareTo(new Double(perimetroPlanet)) < 0 ) {
+					maxPerimetro = new Double(perimetroPlanet);
+				}
+			}
+			
 			if(Math.signum(areaPlanet) != signumPrevPlanet && Math.signum(areaPlanet) != 0f) {
 				signumPrevPlanet = Math.signum(areaPlanet);
-				/*
-				System.out.println("Dia : " + n);
-				System.out.println("Planeta a: " + pA.getPoint2D(n));
-				System.out.println("Planeta b: " + pB.getPoint2D(n));
-				System.out.println("Planeta c: " + pC.getPoint2D(n));
-				*/
 				if(Math.signum(areaSun) != signumPrevSun && Math.signum(areaSun) != 0f) {
 					signumPrevSun = Math.signum(areaSun);
 					countSequia++;
@@ -67,15 +71,11 @@ public class AreaCalculatorTest {
 				signumPrevSun = Math.signum(areaSun);
 			}
 			
-			//Max perimetro del triangulo
-			if(maxPerimetro.compareTo(new Double(perimetroPlanet)) < 0 ) {
-				maxPerimetro = new Double(perimetroPlanet);
-			}
-			
 			
 		}
 		System.out.println("Veces que hubo condiciones optimas: " + countCollinear);
 		System.out.println("Veces que hubo sequia: " + countSequia);
+		System.out.println("Dias lluvia: " + countDiasLluvia);
 		System.out.println("Perimetro maximo: " + maxPerimetro);
 	}
 
